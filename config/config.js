ab7769ac8fb54313632e06c4824b8ff0b55a35f9
HizkiaHasibuan <hizkiahasibuan1@gmail.com>
2023-12-12 14:39:24 +0700

    test1

--- config/config.js
+++ config/config.js
@@ -7,28 +7,28 @@ var con = mysql.createConnection({
     database: process.env.railway
 });
 
-con.connect(function (err) {
-    if (err) throw err;
-    let queryDb = `CREATE DATABASE IF NOT EXISTS db_restaurant`
-    con.query(queryDb, function(err, result, fields) {
-        if (err) throw err;
-    });
-    let queryTbMenu = `CREATE TABLE IF NOT EXISTS ${'`railway`'}.tb_menu (
-        id int NOT NULL AUTO_INCREMENT,
-        nama varchar(255) NOT NULL,
-        harga int NOT NULL,
-        kategori enum ('makanan ringan', 'makanan berat', 'minuman') NOT NULL,
-        stok int NOT NULL,
-        deskripsi text NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        updated_at TIMESTAMP,
-        deleted_at TIMESTAMP,
-        PRIMARY KEY(id)
-    )`
-    con.query(queryTbMenu, function(err, result, fields) {
-        if (err) throw err;
-    });
-    console.log("Database connected!");
-})
+// con.connect(function (err) {
+//     if (err) throw err;
+//     let queryDb = `CREATE DATABASE IF NOT EXISTS db_restaurant`
+//     con.query(queryDb, function(err, result, fields) {
+//         if (err) throw err;
+//     });
+//     let queryTbMenu = `CREATE TABLE IF NOT EXISTS ${'`railway`'}.tb_menu (
+//         id int NOT NULL AUTO_INCREMENT,
+//         nama varchar(255) NOT NULL,
+//         harga int NOT NULL,
+//         kategori enum ('makanan ringan', 'makanan berat', 'minuman') NOT NULL,
+//         stok int NOT NULL,
+//         deskripsi text NOT NULL,
+//         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+//         updated_at TIMESTAMP,
+//         deleted_at TIMESTAMP,
+//         PRIMARY KEY(id)
+//     )`
+//     con.query(queryTbMenu, function(err, result, fields) {
+//         if (err) throw err;
+//     });
+//     console.log("Database connected!");
+// })
 
 module.exports = con;
