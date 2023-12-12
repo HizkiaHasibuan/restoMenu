9b567dc199a80931028856dd16f35576f6e0a419
HizkiaHasibuan <hizkiahasibuan1@gmail.com>
2023-12-12 16:18:03 +0700

    test13

--- config/config.js
+++ config/config.js
@@ -4,7 +4,8 @@ var con = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DATABSE
+    database: process.env.DATABSE,
+    port: process.env.DB_PORT
 });
 
 module.exports = con;
